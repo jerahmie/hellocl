fdcbe344679c5065ccc4e87bcd8e2a619fc4e355
jerahmie <jerahmie@gmail.com>
2022-07-02 09:35:04 -0500

    can now print opencl device info

--- hellocl.cpp
+++ hellocl.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 #define __CL_ENABLE_EXCEPTIONS
-#include <CL/cl.hpp>
+#include <CL/cl2.hpp>
 
 // compute c = a + b
 static const char source[] =
@@ -31,7 +31,7 @@ static const char source[] =
 int main(void)
 {
   const size_t N = 1 << 20;
-  
+  std::cout << "\n\t\t" <<  " << Hello OpenCL >> " << "\n\n";
   try {
     // Get list of OpenCL platforms.
     std::vector<cl::Platform> platform;
@@ -40,6 +40,24 @@ int main(void)
     if (platform.empty()) {
       std::cerr << "OpenCL platforms not found." << '\n';
       return 1;
+    }
+    for (const auto& p: platform) {
+      std::string platform_name;
+      p.getInfo(CL_PLATFORM_NAME, &platform_name);
+      std::string platform_vendor;
+      p.getInfo(CL_PLATFORM_VENDOR, &platform_vendor);
+      std::string platform_extensions;
+      p.getInfo(CL_PLATFORM_EXTENSIONS, &platform_extensions);
+      std::string platform_profile;
+      p.getInfo(CL_PLATFORM_PROFILE, &platform_profile);
+      std::string platform_version;
+      p.getInfo(CL_PLATFORM_VERSION, &platform_version);
+      
+      std::cout << '\t' <<   "      CL_PLATFORM_NAME: " << platform_name << '\n';
+      std::cout << '\t' <<   "    CL_PLATFORM_VENDOR: " << platform_vendor << '\n';
+      std::cout << '\t' <<   "CL_PLATFORM_EXTENSIONS: " << platform_extensions << '\n';
+      std::cout << '\t' <<   "   CL_PLATFORM_PROFILE: " << platform_profile << '\n';
+      std::cout << '\t' <<   "   CL_PLATFORM_VERSION: " << platform_version << '\n';
     }
   } catch (const cl::Error &err) {
     std::cerr
