4b3a77eeef5bc4eb3571f330e5d2a7f184fd669f
Jerahmie Radder <jerahmie@gmail.com>
2022-07-05 00:24:59 -0500

    adding platform commands

--- hellocl-rs/src/main.rs
+++ hellocl-rs/src/main.rs
@@ -1,9 +1,43 @@
 /*
  * Explore OpenCL with Rust
  */
+use std::io;
+use std::io::Write;
+use opencl3::platform::*;
+use opencl3::error_codes::*;
+use cl3::device::*;
 
-use opencl3::*; 
+fn platform_display(id: usize, p: &Platform) ->() {
+    println!("     -------------- OpenCL Device Summary -------------------");
+    println!("\tPlatform Number: {}", id);
+    println!("\tPlatform Name: {}", p.name().unwrap());
+    println!("\tPlatform Profile: {}", p.profile().unwrap());
+    println!("\tPlatform Version: {}", p.version().unwrap());
+    println!("\tPlatform Vendor: {}", p.vendor().unwrap());
+    println!("\tPlatform Extensions: {}", p.extensions().unwrap());
+    //println!("\tPlatform Host Timer Resulution: {}", p.host_timer_resolution().unwrap());
+    //println!("\tPlatform Numeric Version: {}", p.numeric_version().unwrap());
+    //println!("\tPlatform Extensions with Version: {}", p.extensions_with_version().unwrap());
+    println!("\tPlatform External Memory Import Handle Types KHR: \n\t\t");
+    
+    let handle_types = p.platform_external_memory_import_handle_types_khr();
+    let handle_types = match handle_types {
+	Ok(types) => types,
+	Err(error) => panic!("--> {:?}", error_text(error.0)),
+    };
+    for h in handle_types {
+	print!("{}, ", h.version);
+	//io::stdout().flush().unwrap();
+    }
+    
+}
 
 fn main() {
-    println!("Hello, world!");
+    let platforms = get_platforms().unwrap();
+    assert!(0 < platforms.len());
+    
+    println!("Number OpenCL platforms found: {}", &platforms.len());
+    for (i, p) in platforms.iter().enumerate() {
+	platform_display(i, p);
+    }
 }
---
+++ src/hellocl.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include <CL/cl2.hpp>
+
+// compute c = a + b
+static const char source[] =
+  "#if define(dcl_khr_fp64)\n"
+  "#  pragma OPENCL EXTENSION cl_khr_fp64: enable\n"
+  "#elif defined(cl_amp_fp64)\n"
+  "#  pragma OPENCL EXTENSION cl_amp_fp64: enable\n"
+  "#else\n"
+  "#  error double precision is not supported\n"
+  "#endif\n"
+  "kernel void add(\n"
+  "       ulong n,\n"
+  "       global const double *a,\n"
+  "       global const double *b,\n"
+  "       global double *c\n"
+  "       )\n"
+  "{\n"
+  "    size_t i = get_global_id(0);\n"
+  "    if (i < n) {\n"
+  "       c[i] = a[i] + b[i];\n"
+  "    }\n"
+  "}\n";
+  
+
+void print_ocl_device_info(cl::Device device) {
+  std::cout << "CL Device Info..." << '\n';
+  
+}
+
+int main(void)
+{
+  const size_t N = 1 << 20;
+  
+  std::cout << "\n\t\t" <<  " << Hello OpenCL >> " << "\n\n";
+  try {
+    // Get list of OpenCL platforms.
+    std::vector<cl::Platform> platform;
+    cl::Platform::get(&platform);
+
+    if (platform.empty()) {
+      std::cerr << "OpenCL platforms not found." << '\n';
+      return 1;
+    }
+    // print platform info
+    std::cout << "\n\t\t" << " << OpenCL Devices >> " << "\n\n";
+    for (const auto& p: platform) {
+      std::string platform_name;
+      p.getInfo(CL_PLATFORM_NAME, &platform_name);
+      std::string platform_vendor;
+      p.getInfo(CL_PLATFORM_VENDOR, &platform_vendor);
+      std::string platform_extensions;
+      p.getInfo(CL_PLATFORM_EXTENSIONS, &platform_extensions);
+      std::string platform_profile;
+      p.getInfo(CL_PLATFORM_PROFILE, &platform_profile);
+      std::string platform_version;
+      p.getInfo(CL_PLATFORM_VERSION, &platform_version);
+      
+      std::cout << '\t' <<   "      CL_PLATFORM_NAME: " << platform_name << '\n';
+      std::cout << '\t' <<   "    CL_PLATFORM_VENDOR: " << platform_vendor << '\n';
+      std::cout << '\t' <<   "CL_PLATFORM_EXTENSIONS: " << platform_extensions << '\n';
+      std::cout << '\t' <<   "   CL_PLATFORM_PROFILE: " << platform_profile << '\n';
+      std::cout << '\t' <<   "   CL_PLATFORM_VERSION: " << platform_version << '\n';
+      
+      int  status = 0;
+      std::vector<cl::Device> devices;
+    
+      status = p.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+      if (status != CL_SUCCESS) {
+	return status;
+      }
+      // print device info
+      if (devices.empty()) {
+	std::cerr << "\n No OpenCL devices found." << '\n';
+	return 1;
+      }
+
+    }
+
+  } catch (const cl::Error &err) {
+    std::cerr
+      << "OpenCL error: "
+      << err.what() << "(" << err.err() << ")"
+      << std::endl;
+    return 1;
+  }
+}
