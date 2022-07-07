2affc74c95730bb5f32e95a1a0d9f93fa0f86a10
Jerahmie Radder <jerahmie@gmail.com>
2022-07-07 08:37:03 -0500

    probing OpenCL devices

--- hellocl-rs/src/main.rs
+++ hellocl-rs/src/main.rs
@@ -1,13 +1,13 @@
 /*
  * Explore OpenCL with Rust
  */
-use std::io;
-use std::io::Write;
-use std::str;
+//use std::io;
+//use std::io::Write;
+//use std::str;
 use opencl3::platform::*;
-use opencl3::error_codes::*;
-use cl3::device::*;
+//use opencl3::error_codes::*;
 use cl3::platform::*;
+use cl3::device::*;
 
 fn platform_display(id: usize, p: &Platform) ->() {
     println!("     -------------- OpenCL Device Summary -------------------");
@@ -31,18 +31,41 @@ fn platform_display(id: usize, p: &Platform) ->() {
     //	//io::stdout().flush().unwrap();
     //}
     let platform_name = p.get_data(CL_PLATFORM_NAME).unwrap();
-    // from_utf8
-    let platform_name: String = String::from_ascii(platform_name.clone()).unwrap();
-    println!("\tplatform_data: {}", platform_name.trim());
+    let platform_name: String = String::from_utf8(platform_name.clone()).unwrap();
+    println!("\tCL_PLATFORM_NAME: {}", platform_name.trim());
+    let platform_vendor = p.get_data(CL_PLATFORM_VENDOR).unwrap();
+    let platform_vendor: String = String::from_utf8(platform_vendor.clone()).unwrap();
+    println!("\tCL_PLATFORM_VENDOR: {}", platform_vendor.trim());
+    let platform_profile = p.get_data(CL_PLATFORM_PROFILE).unwrap();
+    let platform_profile: String = String::from_utf8(platform_profile.clone()).unwrap();
+    println!("\tCL_PLATFORM_PROFILE: {}", platform_profile.trim());
+    let platform_version = p.get_data(CL_PLATFORM_VERSION).unwrap();
+    let platform_version: String = String::from_utf8(platform_version.clone()).unwrap();
+    println!("\tCL_PLATFORM_VERSION: {}", platform_version.trim());
+    let platform_info: Vec<&str> = platform_version.split_whitespace().collect();
+    println!("\tOpenCL version: {}", platform_info[1]);
+}
 
+fn device_display(p: &Platform){
+    let device_ids = p.get_devices(CL_DEVICE_TYPE_GPU).unwrap();
+    println!("\tGPU devices: {:?}", device_ids);
+    let device_ids = p.get_devices(CL_DEVICE_TYPE_CPU).unwrap();
+    println!("\tCPU devices: {:?}", device_ids);
+    let device_info = get_device_info(device_ids[0], CL_DEVICE_TYPE).unwrap();
+    let device_info: cl_ulong = cl_ulong::from(device_info); 
+    println!("\t{:?}", device_info);
 }
 
 fn main() {
     let platforms = get_platforms().unwrap();
     assert!(0 < platforms.len());
-    
+
+    println!("----------------------------------------------------");
     println!("Number OpenCL platforms found: {}", &platforms.len());
+    
     for (i, p) in platforms.iter().enumerate() {
 	platform_display(i, p);
+	println!("\n --- \n");
+	device_display(p);
     }
 }
